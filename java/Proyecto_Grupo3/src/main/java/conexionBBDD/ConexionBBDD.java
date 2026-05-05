28e63e57bd60cdb1b3c15b78ba2255f38917aea8
devcodery <devcoderys@gmail.com>
2026-05-05 19:57:56 +0200

    refactor: eliminar conexión estática y métodos obsoletos, utilizando dotenv para la configuración de la base de datos

--- java/Proyecto_Grupo3/src/main/java/conexionBBDD/ConexionBBDD.java
+++ java/Proyecto_Grupo3/src/main/java/conexionBBDD/ConexionBBDD.java
@@ -3,17 +3,12 @@ package conexionBBDD;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.SQLTimeoutException;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConexionBBDD {
 	private static final String DRIVER = "org.postgresql.Driver";
-	private static final String URL = "jdbc:postgresql://localhost:5432/proyecto-grupo3";
-	private static final String USUARIO = "postgres";
-	private static final String PASSWORD = "1234";
 	private Connection conexion = null;
-
 
 	private static final Dotenv DOTENV = Dotenv.configure().ignoreIfMissing().load();
     private static final String HOST = DOTENV.get("DB_HOST");
@@ -22,24 +17,6 @@ public class ConexionBBDD {
     private static final String URL_DOTENV = "jdbc:postgresql://"+HOST+":"+PUERTO+"/"+DB;
     private static final String USER = DOTENV.get("DB_USER");
     private static final String PASS = DOTENV.get("DB_PASSWORD");
-
-	public Connection conectarBDPostgreSQL() {
-		try {
-			Class.forName(DRIVER);
-			this.conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
-			System.out.println("Conexion a BBDD PostgresSQL OK");
-		} catch (ClassNotFoundException var2) {
-			var2.printStackTrace();
-		} catch (SQLTimeoutException var3) {
-			System.err.println("ERROR ha pasado el tiempo de conexion");
-			var3.printStackTrace();
-		} catch (SQLException var4) {
-			System.err.println("ERROR en la conexion a BBDD PostgresSQL");
-			var4.printStackTrace();
-		}
-
-		return this.conexion;
-	}
 	
 	public Connection conectarBDDotenv() {
         try {
