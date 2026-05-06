ced70d9a9c91241eff3ed9f38e9112dcdc8d0cca
devcodery <devcoderys@gmail.com>
2026-05-06 23:33:37 +0200

    feat: agregar campos adicionales en el formulario de registro y mejorar la lógica de inicio de sesión

--- python/app.py
+++ python/app.py
@@ -2,10 +2,11 @@ from datetime import timedelta
 from functools import wraps
 from urllib.parse import urlparse
 import os
-
 import psycopg2
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+
+import requests
 
 load_dotenv()
 
@@ -40,19 +41,37 @@ def login_usuario(user, passwd):
     with get_conn() as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT login_usuario(%s, %s);", (user, passwd))
-            return cur.fetchone()[0]
+            resultado = cur.fetchone()
+            
+            if resultado is None:
+                return False, None 
+            
+            login_correcto = resultado[0]
+            
+            if login_correcto:
+                cur.execute("select * from usuario where nombre = %s or correo = %s;",(user, user))
+                userComplety = cur.fetchall()[0]
+                
+                return login_correcto, userComplety
+            else:
+                return login_correcto, None
+                
 
 
-def registrar_usuario(nombre, correo, passwd):
+def registrar_usuario(nombre, correo, passwd, apellidos, rol, direccion, departamento, sede):
     """Registra un usuario normal usando la función SQL registrar_usuario."""
     with get_conn() as conn:
         with conn.cursor() as cur:
             cur.execute(
-                "SELECT registrar_usuario(%s, %s, %s);",
-                (nombre, correo, passwd),
+                "SELECT registrar_usuario(%s, %s, %s, %s, %s, %s, %s, %s);",
+                (nombre, apellidos, correo, passwd, rol, direccion, departamento, sede),
             )
-            return cur.fetchone()[0]
-
+            resultado = cur.fetchone()
+            
+            if resultado is None:
+                return False
+            
+            return resultado[0]
 
 def login_requerido(f):
     """Decorador para proteger rutas propias de Flask."""
@@ -77,24 +96,62 @@ def next_url_segura(next_url):
     return parsed.netloc == ""
 
 
-@app.route("/form", methods=["GET"])
-def form():
-    """Formulario sencillo de registro."""
-    return render_template("formulario.html")
+@app.route("/form/<string:rol>", methods=['GET'])
+def form(rol):
 
+    print("Has entrado en el formulario")
+
+    # Obtener sedes
+    # url = "http://info.empresa.dam.es:8055/items/sedes"
+    # respuesta = requests.get(url)
+    # datosSedes = respuesta.json()
+
+    # Obtener departamentos
+    # url = "http://info.empresa.dam.es:8055/items/departamentos"
+    # respuesta = requests.get(url)
+    # datosDepartamentos = respuesta.json()
+
+    return render_template(
+        "formulario.html",
+        # departamentos=datosDepartamentos['data'],
+        # sedes=datosSedes['data'],
+        rol=rol
+    )
 
 @app.route("/registro", methods=["POST"])
 def registro():
     nombre = request.form.get("nombre")
+    apellidos = request.form.get("apellidos")
     correo = request.form.get("email")
     passwd = request.form.get("passwd")
+    rol = request.form.get("rol")
+    direccion = request.form.get("direccion")
+    departamento = request.form.get("departamento")
+    sede = request.form.get("sede")
 
-    if registrar_usuario(nombre, correo, passwd):
+    if registrar_usuario(nombre, correo, passwd, apellidos, rol, direccion, departamento, sede):
         return redirect(url_for("login"))
 
     return "No se pudo registrar el usuario. Revisa si ya existe.", 400
 
+@app.route("/usuarios", methods=["GET"])
+@login_requerido
+def usuarios():
 
+    if session.get("roles") == "admin":
+
+        with get_conn() as conn:
+            with conn.cursor() as cur:
+
+                cur.execute("SELECT * FROM usuario;")
+
+                filas = cur.fetchall()
+
+        return jsonify(filas)
+
+    else:
+        return f"No tienes permisos {session.get('user')} and roles: {session.get('roles')} {session}"
+    
 @app.route("/login", methods=["GET", "POST"])
 def login():
     error = ""
@@ -109,18 +166,24 @@ def login():
             session.permanent = True
             session["login"] = True
             session["user"] = user
-            session["roles"] = ["admin"]
+            session["roles"] = "admin"
 
             if next_url_segura(next_url):
                 return redirect(next_url)
             return redirect(url_for("index"))
 
         # Usuarios normales registrados en PostgreSQL.
-        if login_usuario(user, passwd):
+        login_correcto, userComplety  = login_usuario(user, passwd)
+        
+        if userComplety is None:
+            error = "No se extrajo los datos del usuario."
+            return render_template("login.html", error=error, next_url=next_url)
+        
+        if login_correcto: 
             session.permanent = True
             session["login"] = True
             session["user"] = user
-            session["roles"] = ["cliente"]
+            session["roles"] = userComplety[5] 
 
             if next_url_segura(next_url):
                 return redirect(next_url)
@@ -188,11 +251,13 @@ def auth_verify():
 @app.route("/")
 @login_requerido
 def index():
+    
     return f"""
     <h1>Login correcto</h1>
     <p>Usuario: {session.get("user")}</p>
     <p>Roles: {session.get("roles")}</p>
-    <p><a href="/form">Registrar usuario</a></p>
+    
+    <p><a href="/usuarios">Registrar usuario</a></p>
     <p><a href="/logout">Cerrar sesión</a></p>
     """
 
