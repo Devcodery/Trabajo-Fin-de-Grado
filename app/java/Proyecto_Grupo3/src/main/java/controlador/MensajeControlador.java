28c1944357e84273a6722159c423663da6b8ed6d
Caua <caua.hino@gmail.com>
2026-05-19 15:44:16 +0200

    fix: actualizar redirección a la vista de gestión de mensajes en MensajeControlador.java

--- app/java/Proyecto_Grupo3/src/main/java/controlador/MensajeControlador.java
+++ app/java/Proyecto_Grupo3/src/main/java/controlador/MensajeControlador.java
@@ -70,7 +70,7 @@ public class MensajeControlador extends HttpServlet {
 			String idUsuarioStr = (String)session.getAttribute("idUsuario");
 			int idUsuario = Integer.valueOf(idUsuarioStr);
 			mensajeDAO.create(idConsulta, idUsuario, descripcion, asunto);	
-			request.getRequestDispatcher("/vistas/verMensajes.jsp").forward(request, response);
+			request.getRequestDispatcher("/vistas/gestionMensajes.jsp").forward(request, response);
 		}
 		conexion.cerrarConexion();
 	}
