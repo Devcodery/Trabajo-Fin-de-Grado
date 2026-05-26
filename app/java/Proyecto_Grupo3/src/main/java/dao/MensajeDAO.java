58a78a7b12e16e940d34017e4f56f85e04cf2f78
Caua <caua.hino@gmail.com>
2026-05-26 17:57:04 +0200

    fix: arreglo en el metodo de readMensaje

--- app/java/Proyecto_Grupo3/src/main/java/dao/MensajeDAO.java
+++ app/java/Proyecto_Grupo3/src/main/java/dao/MensajeDAO.java
@@ -62,7 +62,7 @@ public class MensajeDAO {
 			sentencia.setInt(2, idUsuario);
 			ResultSet rs = sentencia.executeQuery();
 			while(rs.next()) {
-				mensajes.add(new Mensaje(rs.getInt("id_mensajeria"), rs.getInt("id_usuario"), rs.getInt("id_consulta"), rs.getString("asunto"), rs.getString("descripcion")));
+				mensajes.add(new Mensaje(rs.getInt("id_mensajeria"), rs.getInt("id_usuario"), rs.getInt("id_consulta"), rs.getString("asunto"), rs.getString("descripcion"), rs.getString("prioridad")));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
